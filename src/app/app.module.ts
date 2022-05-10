486ead6437f9bb7b5df052b99e728fb5bedef3b7
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2022-05-10 20:52:59 -0300

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
+import { NavComponent } from './primeiro-componente/nav/nav.component';
+import { FooterComponent } from './primeiro-componente/footer/footer.component';
+import { ContentComponent } from './primeiro-componente/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponenteComponent,
+    NavComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
