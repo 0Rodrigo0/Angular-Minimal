8c38864c4d299399f95c513b65ee650f1131f28e
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2022-05-15 16:18:43 -0300

    btn login e signin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { FooterComponent } from "./primeiro-componente/footer/footer.component";
 import { ContentComponent } from "./primeiro-componente/content/content.component";
 import { LandingPageComponent } from "./primeiro-componente/landing-page/landing-page.component";
 import { CarrousselComponent } from "./primeiro-componente/carroussel/carroussel.component";
+import { ButtonsComponent } from "./primeiro-componente/buttons/buttons.component";
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { CarrousselComponent } from "./primeiro-componente/carroussel/carroussel
     ContentComponent,
     LandingPageComponent,
     CarrousselComponent,
+    ButtonsComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
