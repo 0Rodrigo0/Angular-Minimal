3c29d727180f498e4dc8cad07430b55cd3aba8c7
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2022-05-26 19:53:27 -0300

    login e signin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { ContentComponent } from "./primeiro-componente/content/content.component";
+import { SignInComponent } from "./primeiro-componente/sign-in/sign-in.component";
+import { LoginPageComponent } from "./primeiro-componente/login-page/login-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: ContentComponent },
+  { path: "sign-in", component: SignInComponent },
+  { path: "login", component: LoginPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
